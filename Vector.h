feee9a5352fd3c3dfcca79b26e222adaebe8908f
Not born to die <3490241794@qq.com>
2022-09-27 03:10:42 -0700

    first

---
+++ Vector.h
@@ -0,0 +1,24 @@
+template <typename T>
+class Vector
+{
+private:
+
+    /* data */
+public:
+
+    Vector(
+        /* args */);
+    ~Vector();
+};
+
+template<typename T>
+Vector<T>::Vector(/* args */)
+{template
+
+
+}
+
+template<typename T>
+Vector<T>::~Vector()
+{
+}
